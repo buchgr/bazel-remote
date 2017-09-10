da9ac3121ff09f313746010d3e6b392c136ba684
Jakob Buchgraber <buchgr@google.com>
2017-09-10 17:31:53 +0200

    httptest: check for purging

--- cache/http_test.go
+++ cache/http_test.go
@@ -69,8 +69,8 @@ func TestUploadFilesConcurrently(t *testing.T) {
 		requests[i] = r
 	}
 
-	e := NewEnsureSpacer(0.3, 0.1)
-	h := NewHTTPCache(cacheDir, 10000*1024, e)
+	e := NewEnsureSpacer(0.8, 0.5)
+	h := NewHTTPCache(cacheDir, 1000*1024, e)
 	handler := http.HandlerFunc(h.CacheHandler)
 
 	var wg sync.WaitGroup
@@ -91,4 +91,28 @@ func TestUploadFilesConcurrently(t *testing.T) {
 	}
 
 	wg.Wait()
+
+	f, err := os.Open(cacheDir)
+	defer f.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	files, err := f.Readdir(-1)
+	if err != nil {
+		t.Error(err)
+	}
+	var totalSize int64
+	for _, fileinfo := range files {
+		size := fileinfo.Size()
+		if size != 1024 {
+			t.Error("Expected all files to be 1024 bytes.", "Got", size)
+		}
+		totalSize += fileinfo.Size()
+	}
+
+	// Test that purging worked and kept cache size in check.
+	if upperBound := int64(NumUploads) * 1024 * 1024; totalSize > upperBound {
+		t.Error("Cache size too big. Expected at most", upperBound, "got",
+			totalSize)
+	}
 }
