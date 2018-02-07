69d61697cd35736381ede80eea49184222091b90
David Schile <david.v.schile@nordstrom.com>
2018-02-07 12:46:01 -0800

    added basic auth

--- main.go
+++ main.go
@@ -6,16 +6,21 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/buchgr/bazel-remote/cache"
+	"github.com/bajacondor/bazel-remote/cache"
+	//"github.com/buchgr/bazel-remote/cache"
 )
 
 func main() {
-  host := flag.String("host", "", "Host to bind the http server")
+	host := flag.String("host", "", "Host to bind the http server")
 	port := flag.Int("port", 8080, "The port the HTTP server listens on")
 	dir := flag.String("dir", "",
 		"Directory path where to store the cache contents")
 	maxSize := flag.Int64("max_size", -1,
 		"The maximum size of the remote cache in GiB")
+	user := flag.String("user", "",
+		"Username for basic authentication")
+	pass := flag.String("pass", "",
+		"Password for basic authentication")
 	flag.Parse()
 
 	if *maxSize <= 0 {
@@ -27,7 +32,24 @@ func main() {
 	h := cache.NewHTTPCache(*dir, *maxSize*1024*1024*1024, e)
 	s := &http.Server{
 		Addr:    *host + ":" + strconv.Itoa(*port),
-		Handler: http.HandlerFunc(h.CacheHandler),
+		Handler: http.HandlerFunc(auth(h.CacheHandler, *user, *pass)),
 	}
 	log.Fatal(s.ListenAndServe())
+}
+
+func auth(fn http.HandlerFunc, eu, ep string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if ep != "" {
+			user, pass, _ := r.BasicAuth()
+			if !check(user, pass, eu, ep) {
+				http.Error(w, "Unauthorized.", 401)
+				return
+			}
+		}
+		fn(w, r)
+	}
+}
+
+func check(u, p, eu, ep string) bool {
+	return u == eu && p == ep
 }
