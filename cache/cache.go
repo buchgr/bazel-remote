fea9ac0c8413bbd97692cb3604c5457739a3e881
nicolov <nicolov@users.noreply.github.com>
2018-06-04 21:39:40 -0700

    Allow overwriting existing items (#43)

--- cache/cache.go
+++ cache/cache.go
@@ -129,14 +129,16 @@ func (c *fsCache) loadExistingFiles() {
 func (c *fsCache) Put(key string, size int64, expectedSha256 string, r io.Reader) (err error) {
 	c.mux.Lock()
 
-	// If `key` is already in the LRU, don't touch the cache and just discard
-	// the incoming stream. This applies to both committed an uncommitted files
-	// (we don't want to upload again if an upload of the same file is already
-	// in progress).
-	if _, found := c.lru.Get(key); found {
-		c.mux.Unlock()
-		io.Copy(ioutil.Discard, r)
-		return
+	// If there's an ongoing upload (i.e. cache key is present in uncommitted state),
+	// we drop the upload and discard the incoming stream. We do accept uploads
+	// of existing keys, as it should happen relatively rarely (e.g. race
+	// condition on the bazel side) but it's useful to overwrite poisoned items.
+	if existingItem, found := c.lru.Get(key); found {
+		if !existingItem.(*lruItem).committed {
+			c.mux.Unlock()
+			io.Copy(ioutil.Discard, r)
+			return
+		}
 	}
 
 	// Try to add the item to the LRU.
