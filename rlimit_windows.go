17df974013d02645ca3b4f8665fc69bfd4fccbd0
Mostyn Bramley-Moore <mostyn@antipode.se>
2020-02-13 07:57:06 +0100

    automatically raise the limit on the number of open files
    bazel-remote opens a lot of files, and can easily hit the limit unless
    it is raised. Let's try to do that automatically.
    
    This requires a workaround on macos, where getrlimit does not return
    the correct hard limit for RLIMIT_NOFILE. We need to use the minimum
    of the value from getrlimit and from sysctl. Since cgo is difficult
    with bazel's rules_go just exec the sysctl command.
    
    Fixes #174.
    
    You might need to take additional steps to raise the hard limit,
    depending on your operating system's defaults.

---
+++ rlimit_windows.go
@@ -0,0 +1,10 @@
+// +build windows
+
+package main
+
+// On unix, we need to raise the limit on the number of open files.
+// Unsure if anything is required on windows, or if bazel-remote even
+// works on windows. But let's not intentionally prevent compiling
+// for windows.
+func adjustRlimit() {
+}
