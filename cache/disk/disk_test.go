buchgr/bazel-remote

commit b2e2f705f43d20e12dd8139f1719e7bd9c40a9b2

fix some go vet shadow warnings
To see the warnings:
go install golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
go vet -vettool=$(which shadow) ./...

The shadow checker is considered "experimental", and some projects
find it noisy. So I don't think we should add such a check to the
bazelci presubmit setup for now.

cache/disk/disk.go
@@ -342,11 +342,11 @@ func (c *DiskCache) Put(kind cache.EntryKind, hash string, expectedSize int64, r
 		}
 	}
 
-	if err := f.Sync(); err != nil {
+	if err = f.Sync(); err != nil {
 		return err
 	}
 
-	if err := f.Close(); err != nil {
+	if err = f.Close(); err != nil {
 		return err
 	}
 
@@ -420,12 +420,14 @@ func (c *DiskCache) Get(kind cache.EntryKind, hash string) (io.ReadCloser, int64
 
 	if available {
 		blobPath := cacheFilePath(kind, c.dir, hash)
-		fileInfo, err := os.Stat(blobPath)
+		var fileInfo os.FileInfo
+		fileInfo, err = os.Stat(blobPath)
 		if err == nil {
-			r, err := os.Open(blobPath)
+			var f *os.File
+			f, err = os.Open(blobPath)
 			if err == nil {
 				cacheHits.Inc()
-				return r, fileInfo.Size(), nil
+				return f, fileInfo.Size(), nil
 			}
 		}
 
@@ -501,7 +503,7 @@ func (c *DiskCache) Get(kind cache.EntryKind, hash string) (io.ReadCloser, int64
 		return nil, -1, err
 	}
 
-	if err := f.Close(); err != nil {
+	if err = f.Close(); err != nil {
 		return nil, -1, err
 	}
 
@@ -512,7 +514,8 @@ func (c *DiskCache) Get(kind cache.EntryKind, hash string) (io.ReadCloser, int64
 		// This flag is used by the defer() block above.
 		shouldCommit = true
 
-		f2, err := os.Open(filePath)
+		var f2 *os.File
+		f2, err = os.Open(filePath)
 		if err == nil {
 			return f2, foundSize, nil
 		}
@@ -639,7 +642,7 @@ func (c *DiskCache) GetValidatedActionResult(hash string) (*pb.ActionResult, []b
 				d.TreeDigest.SizeBytes, size)
 		}
 
-		data, err := ioutil.ReadAll(r)
+		data, err = ioutil.ReadAll(r)
 		r.Close()
 		if err != nil {
 			return nil, nil, err

cache/http/http.go
@@ -140,7 +140,8 @@ func (r *remoteHTTPProxyCache) Get(kind cache.EntryKind, hash string) (io.ReadCl
 	if rsp.StatusCode != http.StatusOK {
 		// If the failed http response contains some data then
 		// forward up to 1 KiB.
-		errorBytes, err := ioutil.ReadAll(io.LimitReader(rsp.Body, 1024))
+		var errorBytes []byte
+		errorBytes, err = ioutil.ReadAll(io.LimitReader(rsp.Body, 1024))
 		var errorText string
 		if err == nil {
 			errorText = string(errorBytes)

main.go
@@ -231,7 +231,8 @@ func main() {
 			}
 		} else if c.HTTPBackend != nil {
 			httpClient := &http.Client{}
-			baseURL, err := url.Parse(c.HTTPBackend.BaseURL)
+			var baseURL *url.URL
+			baseURL, err = url.Parse(c.HTTPBackend.BaseURL)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -274,20 +275,20 @@ func main() {
 				opts := []grpc.ServerOption{}
 
 				if len(c.TLSCertFile) > 0 && len(c.TLSKeyFile) > 0 {
-					creds, err := credentials.NewServerTLSFromFile(
+					creds, err2 := credentials.NewServerTLSFromFile(
 						c.TLSCertFile, c.TLSKeyFile)
-					if err != nil {
-						log.Fatal(err)
+					if err2 != nil {
+						log.Fatal(err2)
 					}
 					opts = append(opts, grpc.Creds(creds))
 				}
 
 				log.Printf("Starting gRPC server on address %s", addr)
 
-				err = server.ListenAndServeGRPC(addr, opts,
+				err3 := server.ListenAndServeGRPC(addr, opts,
 					diskCache, accessLogger, errorLogger)
-				if err != nil {
-					log.Fatal(err)
+				if err3 != nil {
+					log.Fatal(err3)
 				}
 			}()
 		}

server/grpc_bytestream.go
@@ -242,9 +242,9 @@ func (s *grpcServer) Write(srv bytestream.ByteStream_WriteServer) error {
 			req, err := srv.Recv()
 			if err == io.EOF {
 				if resp.CommittedSize != size {
-					err := fmt.Errorf("Unexpected amount of data read: %d expected: %d",
+					msg := fmt.Sprintf("Unexpected amount of data read: %d expected: %d",
 						resp.CommittedSize, size)
-					recvResult <- status.Error(codes.Unknown, err.Error())
+					recvResult <- status.Error(codes.Unknown, msg)
 					return
 				}
 

server/grpc_test.go
@@ -73,12 +73,12 @@ func TestMain(m *testing.M) {
 	listener = bufconn.Listen(bufSize)
 
 	go func() {
-		err := ServeGRPC(
+		err2 := ServeGRPC(
 			listener,
 			[]grpc.ServerOption{},
 			diskCache, accessLogger, errorLogger)
-		if err != nil {
-			fmt.Println(err)
+		if err2 != nil {
+			fmt.Println(err2)
 			os.Exit(1)
 		}
 	}()
@@ -157,7 +157,7 @@ func TestGrpcAc(t *testing.T) {
 
 	for _, tc := range badDigestTestCases {
 		r := pb.GetActionResultRequest{ActionDigest: &tc.digest}
-		_, err := acClient.GetActionResult(ctx, &r)
+		_, err = acClient.GetActionResult(ctx, &r)
 		checkBadDigestErr(t, err, tc)
 	}
 
@@ -191,7 +191,7 @@ func TestGrpcAc(t *testing.T) {
 
 	for _, tc := range badDigestTestCases {
 		r := pb.UpdateActionResultRequest{ActionDigest: &tc.digest}
-		_, err := acClient.UpdateActionResult(ctx, &r)
+		_, err = acClient.UpdateActionResult(ctx, &r)
 		checkBadDigestErr(t, err, tc)
 	}
 

commit 8bd3079abbf9d326ee3fd9354b2e049540e1e1ae

add some regression tests for DiskCache.GetValidatedActionResult
This test would have caught the regression introduced by PR #195.
(The regression itself was found and fixed: #198, #199, #200).

cache/disk/disk_test.go
@@ -20,6 +20,9 @@ import (
 	"github.com/buchgr/bazel-remote/cache"
 	cachehttp "github.com/buchgr/bazel-remote/cache/http"
 	"github.com/buchgr/bazel-remote/utils"
+
+	pb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	"github.com/golang/protobuf/proto"
 )
 
 func tempDir(t *testing.T) string {
@@ -632,5 +635,176 @@ func TestHttpProxyBackend(t *testing.T) {
 
 	if bytes.Compare(retrievedData, blob) != 0 {
 		t.Fatalf("Expected '%v' but received '%v", retrievedData, blob)
+	}
+}
+
+// Store an ActionResult with an output directory, then confirm that
+// GetValidatedActionResult returns the original item.
+func TestGetValidatedActionResult(t *testing.T) {
+	cacheDir := testutils.TempDir(t)
+	defer os.RemoveAll(cacheDir)
+
+	testCache := New(cacheDir, 1024*32, nil)
+
+	// Create a directory tree like so:
+	// /bar/foo.txt
+	// /bar/grok.txt
+
+	var err error
+
+	grokData := []byte("grok test data")
+	grokHash := sha256.Sum256(grokData)
+	grokHashStr := hex.EncodeToString(grokHash[:])
+
+	err = testCache.Put(cache.CAS, grokHashStr, int64(len(grokData)),
+		bytes.NewReader(grokData))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fooData := []byte("foo test data")
+	fooHash := sha256.Sum256(fooData)
+	fooHashStr := hex.EncodeToString(fooHash[:])
+
+	err = testCache.Put(cache.CAS, fooHashStr, int64(len(fooData)),
+		bytes.NewReader(fooData))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	barDir := pb.Directory{
+		Files: []*pb.FileNode{
+			&pb.FileNode{
+				Name: "foo.txt",
+				Digest: &pb.Digest{
+					Hash:      fooHashStr,
+					SizeBytes: int64(len(fooData)),
+				},
+			},
+			&pb.FileNode{
+				Name: "grok.txt",
+				Digest: &pb.Digest{
+					Hash:      grokHashStr,
+					SizeBytes: int64(len(grokData)),
+				},
+			},
+		},
+	}
+
+	barData, err := proto.Marshal(&barDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	barDataHash := sha256.Sum256(barData)
+	barDataHashStr := hex.EncodeToString(barDataHash[:])
+
+	err = testCache.Put(cache.CAS, barDataHashStr, int64(len(barData)),
+		bytes.NewReader(barData))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rootDir := pb.Directory{
+		Directories: []*pb.DirectoryNode{
+			&pb.DirectoryNode{
+				Name: "bar",
+				Digest: &pb.Digest{
+					Hash:      barDataHashStr,
+					SizeBytes: int64(len(barData)),
+				},
+			},
+		},
+	}
+
+	rootData, err := proto.Marshal(&rootDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rootDataHash := sha256.Sum256(rootData)
+	rootDataHashStr := hex.EncodeToString(rootDataHash[:])
+
+	err = testCache.Put(cache.CAS, rootDataHashStr, int64(len(rootData)),
+		bytes.NewReader(rootData))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tree := pb.Tree{
+		Root:     &rootDir,
+		Children: []*pb.Directory{&barDir},
+	}
+	treeData, err := proto.Marshal(&tree)
+	if err != nil {
+		t.Fatal(err)
+	}
+	treeDataHash := sha256.Sum256(treeData)
+	treeDataHashStr := hex.EncodeToString(treeDataHash[:])
+
+	err = testCache.Put(cache.CAS, treeDataHashStr, int64(len(treeData)),
+		bytes.NewReader(treeData))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Now add an ActionResult that refers to this tree.
+
+	ar := pb.ActionResult{
+		OutputFiles: []*pb.OutputFile{
+			&pb.OutputFile{
+				Path: "bar/grok.txt",
+				Digest: &pb.Digest{
+					Hash:      grokHashStr,
+					SizeBytes: int64(len(grokData)),
+				},
+			},
+			&pb.OutputFile{
+				Path: "foo.txt",
+				Digest: &pb.Digest{
+					Hash:      fooHashStr,
+					SizeBytes: int64(len(fooData)),
+				},
+			},
+		},
+		OutputDirectories: []*pb.OutputDirectory{
+			&pb.OutputDirectory{
+				Path: "",
+				TreeDigest: &pb.Digest{
+					Hash:      treeDataHashStr,
+					SizeBytes: int64(len(treeData)),
+				},
+			},
+		},
+	}
+	arData, err := proto.Marshal(&ar)
+	if err != nil {
+		t.Fatal(err)
+	}
+	arDataHash := sha256.Sum256([]byte("pretend action"))
+	arDataHashStr := hex.EncodeToString(arDataHash[:])
+
+	err = testCache.Put(cache.AC, arDataHashStr, int64(len(arData)),
+		bytes.NewReader(arData))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Finally, check that the validated+returned data is correct.
+	//
+	// Note: we (sometimes) add metadata in the gRPC/HTTP layer, which
+	// would then return a different ActionResult message. But it's safe
+	// to assume that the value should be returned unchanged by the cache
+	// layer.
+
+	rAR, rData, err := testCache.GetValidatedActionResult(arDataHashStr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(arData, rData) {
+		t.Fatal("Returned ActionResult data does not match")
+	}
+
+	if !proto.Equal(rAR, &ar) {
+		t.Fatal("Returned ActionResult proto does not match")
 	}
 }