1e022c3fbc878800231a805f1866045feb036283
Mostyn Bramley-Moore <mostyn@antipode.se>
2020-03-27 06:36:09 +0100

    add an idle timeout test

---
+++ utils/idle/idle_test.go
@@ -0,0 +1,31 @@
+package idle_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/buchgr/bazel-remote/utils/idle"
+)
+
+func TestIdleTimer(t *testing.T) {
+	tearDown := make(chan struct{})
+	it := idle.NewTimer(time.Second)
+	it.Register(tearDown)
+	it.Start()
+
+	for i := 0; i < 5; i++ {
+		select {
+		case <-time.After(500 * time.Millisecond):
+			it.ResetTimer()
+		case <-tearDown:
+			t.Fatal("unexpected timeout")
+		}
+	}
+
+	select {
+	case <-tearDown:
+		return
+	case <-time.After(2 * time.Second):
+		t.Fatal("expected idle timer to trigger")
+	}
+}
