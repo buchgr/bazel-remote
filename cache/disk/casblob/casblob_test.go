aa16071c60f51a43ff35c9125ae0c14e779fde97
Mostyn Bramley-Moore <mostyn@antipode.se>
2021-08-24 21:39:40 +0200

    Silence linter warning that 'slice' is unused

--- cache/disk/casblob/casblob_test.go
+++ cache/disk/casblob/casblob_test.go
@@ -12,4 +12,8 @@ func TestLenSize(t *testing.T) {
 		// errors all over the place.
 		t.Errorf("len() returns a value larger than 8 bytes")
 	}
+	if len(slice) != 0 {
+		// We should never hit this case.
+		t.Errorf("This should silence linters that think slice is never used")
+	}
 }
