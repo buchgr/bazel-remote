1d11839650709812c8a1a3f31d8ea2b2c1c9e7f2
Mostyn Bramley-Moore <mostyn@antipode.se>
2021-12-29 12:22:02 +0100

    Install and use go 17.5 for go vet/gofmt on buildkite

--- .bazelci/buildkite-install-go.sh
+++ .bazelci/buildkite-install-go.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-wget -o $HOME/go1.16.7.linux-amd64.tar.gz https://golang.org/dl/go1.16.7.linux-amd64.tar.gz 1>&2
-tar -xv -C $HOME -f go1.16.7.linux-amd64.tar.gz 1>&2
+wget -o $HOME/go1.17.5.linux-amd64.tar.gz https://golang.org/dl/go1.17.5.linux-amd64.tar.gz 1>&2
+tar -xv -C $HOME -f go1.17.5.linux-amd64.tar.gz 1>&2
