bacfc473432f2eb7b6b182bca3af8ae34aad4257
Mostyn Bramley-Moore <mostyn@antipode.se>
2022-10-09 23:59:53 +0200

    Refactor main to allow concurrent graceful shutdown of http and grpc servers

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ import (
 
 	auth "github.com/abbot/go-http-auth"
 
-	"github.com/buchgr/bazel-remote/cache"
 	"github.com/buchgr/bazel-remote/cache/disk"
 
 	"github.com/buchgr/bazel-remote/config"
@@ -34,6 +33,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
+	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
 )
 
@@ -95,12 +95,19 @@ func run(ctx *cli.Context) error {
 	httpSem := semaphore.NewWeighted(1)
 	var httpServer *http.Server
 
+	idleTimeoutChan := make(chan struct{}, 1)
+
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		for sig := range sigChan {
+		select {
+		case sig := <-sigChan:
 			log.Printf("Received signal: %s, attempting graceful shutdown", sig)
+		case <-idleTimeoutChan:
+			log.Println("Idle timeout reached, attempting graceful shutdown")
+		}
 
+		go func() {
 			if !grpcSem.TryAcquire(1) {
 				if grpcServer != nil {
 					log.Println("Stopping gRPC server")
@@ -108,25 +115,22 @@ func run(ctx *cli.Context) error {
 					log.Println("gRPC server stopped")
 				}
 			}
+		}()
 
-			// The main goroutine exits when httpServer is stopped, so we
-			// need to wait until the gRPC server is stopped first.
-
+		go func() {
 			if !httpSem.TryAcquire(1) {
 				if httpServer != nil {
 					log.Println("Stopping HTTP server")
 					err := httpServer.Shutdown(context.Background())
 					if err != nil {
 						log.Println("Error occurred while stopping HTTP server:", err)
+					} else {
+						log.Println("HTTP server stopped")
 					}
-
-					// The "HTTP server stopped" log comes from the main goroutine.
 				}
 			}
-		}
+		}()
 	}()
-
-	validateAC := !c.DisableHTTPACValidation
 
 	log.Println("Storage mode:", c.StorageMode)
 	if c.StorageMode == "zstd" {
@@ -153,29 +157,14 @@ func run(ctx *cli.Context) error {
 	}
 	diskCache.RegisterMetrics()
 
-	mux := http.NewServeMux()
-	httpServer = &http.Server{
-		Handler:      mux,
-		ReadTimeout:  c.HTTPReadTimeout,
-		TLSConfig:    c.TLSConfig,
-		WriteTimeout: c.HTTPWriteTimeout,
-	}
-
-	checkClientCertForWrites := c.TLSCaFile != "" && c.AllowUnauthenticatedReads
-	h := server.NewHTTPCache(diskCache, c.AccessLogger, c.ErrorLogger, validateAC,
-		c.EnableACKeyInstanceMangling, checkClientCertForWrites, gitCommit)
+	servers := new(errgroup.Group)
 
 	var htpasswdSecrets auth.SecretProvider
+
 	authMode := "disabled"
-	cacheHandler := h.CacheHandler
 	if c.HtpasswdFile != "" {
 		authMode = "basic"
 		htpasswdSecrets = auth.HtpasswdFileProvider(c.HtpasswdFile)
-		if c.AllowUnauthenticatedReads {
-			cacheHandler = unauthenticatedReadWrapper(cacheHandler, htpasswdSecrets, c.HTTPAddress)
-		} else {
-			cacheHandler = authWrapper(cacheHandler, htpasswdSecrets, c.HTTPAddress)
-		}
 	} else if c.TLSCaFile != "" {
 		authMode = "mTLS"
 	}
@@ -192,7 +181,7 @@ func run(ctx *cli.Context) error {
 	var idleTimer *idle.Timer
 	if c.IdleTimeout > 0 {
 		idleTimer = idle.NewTimer(c.IdleTimeout)
-		cacheHandler = wrapIdleHandler(cacheHandler, idleTimer, c.AccessLogger, httpServer)
+		idleTimer.Register(idleTimeoutChan)
 	}
 
 	acKeyManglingStatus := "disabled"
@@ -201,14 +190,77 @@ func run(ctx *cli.Context) error {
 	}
 	log.Println("Mangling non-empty instance names with AC keys:", acKeyManglingStatus)
 
+	servers.Go(func() error {
+		return startHttpServer(c, &httpServer, htpasswdSecrets, idleTimer, httpSem, diskCache)
+	})
+
+	if c.GRPCAddress != "none" {
+		servers.Go(func() error {
+			return startGrpcServer(c, &grpcServer, htpasswdSecrets, idleTimer, grpcSem, diskCache)
+		})
+	}
+
+	if c.ProfileAddress != "" {
+		go func() {
+			// Allow access to /debug/pprof/ URLs.
+			log.Printf("Starting HTTP server for profiling on address %s",
+				c.ProfileAddress)
+			log.Fatal(`Failed to listen on address: "`, c.ProfileAddress,
+				`": `, http.ListenAndServe(c.ProfileAddress, nil))
+		}()
+	}
+
+	if idleTimer != nil {
+		log.Printf("Starting idle timer with value %v", c.IdleTimeout)
+		idleTimer.Start()
+	}
+
+	return servers.Wait()
+}
+
+func startHttpServer(c *config.Config, httpServer **http.Server,
+	htpasswdSecrets auth.SecretProvider, idleTimer *idle.Timer,
+	httpSem *semaphore.Weighted, diskCache disk.Cache) error {
+
+	mux := http.NewServeMux()
+	*httpServer = &http.Server{
+		Handler:      mux,
+		ReadTimeout:  c.HTTPReadTimeout,
+		TLSConfig:    c.TLSConfig,
+		WriteTimeout: c.HTTPWriteTimeout,
+	}
+
+	checkClientCertForWrites := c.TLSCaFile != "" && c.AllowUnauthenticatedReads
+	validateAC := !c.DisableHTTPACValidation
+	h := server.NewHTTPCache(diskCache, c.AccessLogger, c.ErrorLogger, validateAC,
+		c.EnableACKeyInstanceMangling, checkClientCertForWrites, gitCommit)
+
+	cacheHandler := h.CacheHandler
+	if c.HtpasswdFile != "" {
+		if c.AllowUnauthenticatedReads {
+			cacheHandler = unauthenticatedReadWrapper(cacheHandler, htpasswdSecrets, c.HTTPAddress)
+		} else {
+			cacheHandler = authWrapper(cacheHandler, htpasswdSecrets, c.HTTPAddress)
+		}
+	}
+
+	if c.IdleTimeout > 0 {
+		cacheHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			idleTimer.ResetTimer()
+			cacheHandler(w, r)
+		})
+	}
+
 	if c.EnableEndpointMetrics {
 		metricsMdlw := middleware.New(middleware.Config{
 			Recorder: httpmetrics.NewRecorder(httpmetrics.Config{
 				DurationBuckets: c.MetricsDurationBuckets,
 			}),
 		})
-		mux.Handle("/metrics", middlewarestd.Handler("metrics", metricsMdlw, promhttp.Handler()))
-		mux.Handle("/status", middlewarestd.Handler("status", metricsMdlw, http.HandlerFunc(h.StatusPageHandler)))
+		mux.Handle("/metrics", middlewarestd.Handler("metrics", metricsMdlw,
+			promhttp.Handler()))
+		mux.Handle("/status", middlewarestd.Handler("status", metricsMdlw,
+			http.HandlerFunc(h.StatusPageHandler)))
 		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			middlewarestd.Handler(r.Method, metricsMdlw, http.HandlerFunc(cacheHandler)).ServeHTTP(w, r)
 		})
@@ -217,100 +269,8 @@ func run(ctx *cli.Context) error {
 		mux.HandleFunc("/", cacheHandler)
 	}
 
-	if c.GRPCAddress != "none" {
-		go func() {
-			opts := []grpc.ServerOption{}
-			streamInterceptors := []grpc.StreamServerInterceptor{}
-			unaryInterceptors := []grpc.UnaryServerInterceptor{}
-
-			if c.EnableEndpointMetrics {
-				streamInterceptors = append(streamInterceptors, grpc_prometheus.StreamServerInterceptor)
-				unaryInterceptors = append(unaryInterceptors, grpc_prometheus.UnaryServerInterceptor)
-				grpc_prometheus.EnableHandlingTimeHistogram(grpc_prometheus.WithHistogramBuckets(c.MetricsDurationBuckets))
-			}
-
-			if c.TLSConfig != nil {
-				opts = append(opts, grpc.Creds(credentials.NewTLS(c.TLSConfig)))
-
-				if c.TLSCaFile != "" {
-					streamInterceptors = append(streamInterceptors,
-						server.GRPCmTLSStreamServerInterceptor(c.AllowUnauthenticatedReads))
-					unaryInterceptors = append(unaryInterceptors,
-						server.GRPCmTLSUnaryServerInterceptor(c.AllowUnauthenticatedReads))
-				}
-			}
-
-			if htpasswdSecrets != nil {
-				gba := server.NewGrpcBasicAuth(htpasswdSecrets, c.AllowUnauthenticatedReads)
-				streamInterceptors = append(streamInterceptors, gba.StreamServerInterceptor)
-				unaryInterceptors = append(unaryInterceptors, gba.UnaryServerInterceptor)
-			}
-
-			if idleTimer != nil {
-				it := server.NewGrpcIdleTimer(idleTimer)
-				streamInterceptors = append(streamInterceptors, it.StreamServerInterceptor)
-				unaryInterceptors = append(unaryInterceptors, it.UnaryServerInterceptor)
-			}
-
-			opts = append(opts, grpc.ChainStreamInterceptor(streamInterceptors...))
-			opts = append(opts, grpc.ChainUnaryInterceptor(unaryInterceptors...))
-
-			validateAC := !c.DisableGRPCACDepsCheck
-			validateStatus := "disabled"
-			if validateAC {
-				validateStatus = "enabled"
-			}
-			log.Println("gRPC AC dependency checks:", validateStatus)
-
-			enableRemoteAssetAPI := c.ExperimentalRemoteAssetAPI
-			remoteAssetStatus := "disabled"
-			if enableRemoteAssetAPI {
-				remoteAssetStatus = "enabled"
-			}
-			log.Println("experimental gRPC remote asset API:", remoteAssetStatus)
-
-			network := "tcp"
-			addr := c.GRPCAddress
-			if strings.HasPrefix(c.GRPCAddress, "unix://") {
-				network = "unix"
-				addr = c.GRPCAddress[len("unix://"):]
-			}
-
-			log.Printf("Starting gRPC server on address %s", addr)
-
-			grpcServer = grpc.NewServer(opts...)
-
-			if !grpcSem.TryAcquire(1) {
-				log.Println("bazel-remote is shutting down, not starting gRPC server")
-				return
-			}
-			err3 := server.ListenAndServeGRPC(grpcServer,
-				network, addr,
-				validateAC,
-				c.EnableACKeyInstanceMangling,
-				enableRemoteAssetAPI,
-				diskCache, c.AccessLogger, c.ErrorLogger)
-			if err3 != nil {
-				log.Fatal("gRPC server returned fatal error:", err3)
-			}
-		}()
-	}
-
-	if c.ProfileAddress != "" {
-		go func() {
-			// Allow access to /debug/pprof/ URLs.
-			log.Printf("Starting HTTP server for profiling on address %s",
-				c.ProfileAddress)
-			log.Fatal(`Failed to listen on address: "`, c.ProfileAddress, `": `, http.ListenAndServe(c.ProfileAddress, nil))
-		}()
-	}
-
-	validateStatus := "disabled"
-	if validateAC {
-		validateStatus = "enabled"
-	}
-
 	var ln net.Listener
+	var err error
 	if strings.HasPrefix(c.HTTPAddress, "unix://") {
 		ln, err = net.Listen("unix", c.HTTPAddress[len("unix://"):])
 	} else {
@@ -320,21 +280,21 @@ func run(ctx *cli.Context) error {
 		log.Fatal(`Failed to listen on address: "`, c.HTTPAddress, `": `, err)
 	}
 
-	if idleTimer != nil {
-		log.Printf("Starting idle timer with value %v", c.IdleTimeout)
-		idleTimer.Start()
+	validateStatus := "disabled"
+	if validateAC {
+		validateStatus = "enabled"
 	}
+	log.Println("HTTP AC validation:", validateStatus)
 
 	if len(c.TLSCertFile) > 0 && len(c.TLSKeyFile) > 0 {
-		log.Printf("Starting HTTPS server on address %s", httpServer.Addr)
-		log.Println("HTTP AC validation:", validateStatus)
+		log.Printf("Starting HTTPS server on address %s", (*httpServer).Addr)
 
 		if !httpSem.TryAcquire(1) {
 			log.Println("bazel-remote is shutting down, not starting HTTPS server")
 			return nil
 		}
 
-		err = httpServer.ServeTLS(ln, c.TLSCertFile, c.TLSKeyFile)
+		err = (*httpServer).ServeTLS(ln, c.TLSCertFile, c.TLSKeyFile)
 		if err == http.ErrServerClosed {
 			log.Println("HTTPS server stopped")
 			return nil
@@ -344,40 +304,100 @@ func run(ctx *cli.Context) error {
 	}
 
 	log.Printf("Starting HTTP server on address %s", c.HTTPAddress)
-	log.Println("HTTP AC validation:", validateStatus)
 
 	if !httpSem.TryAcquire(1) {
 		log.Println("bazel-remote is shutting down, not starting HTTP server")
 		return nil
 	}
 
-	err = httpServer.Serve(ln)
+	err = (*httpServer).Serve(ln)
 	if err == http.ErrServerClosed {
-		log.Println("HTTP server stopped")
 		return nil
 	}
 
 	return err
 }
 
-func wrapIdleHandler(handler http.HandlerFunc, idleTimer *idle.Timer, accessLogger cache.Logger, httpServer *http.Server) http.HandlerFunc {
+func startGrpcServer(c *config.Config, grpcServer **grpc.Server,
+	htpasswdSecrets auth.SecretProvider, idleTimer *idle.Timer,
+	grpcSem *semaphore.Weighted, diskCache disk.Cache) error {
 
-	tearDown := make(chan struct{})
-	idleTimer.Register(tearDown)
+	opts := []grpc.ServerOption{}
+	streamInterceptors := []grpc.StreamServerInterceptor{}
+	unaryInterceptors := []grpc.UnaryServerInterceptor{}
 
-	go func() {
-		<-tearDown
-		accessLogger.Printf("Shutting down after idle timeout")
-		err := httpServer.Shutdown(context.Background())
-		if err != nil {
-			fmt.Printf("Error when shutting down http server: %s", err.Error())
+	if c.EnableEndpointMetrics {
+		streamInterceptors = append(streamInterceptors, grpc_prometheus.StreamServerInterceptor)
+		unaryInterceptors = append(unaryInterceptors, grpc_prometheus.UnaryServerInterceptor)
+		grpc_prometheus.EnableHandlingTimeHistogram(grpc_prometheus.WithHistogramBuckets(c.MetricsDurationBuckets))
+	}
+
+	if c.TLSConfig != nil {
+		opts = append(opts, grpc.Creds(credentials.NewTLS(c.TLSConfig)))
+
+		if c.TLSCaFile != "" {
+			streamInterceptors = append(streamInterceptors,
+				server.GRPCmTLSStreamServerInterceptor(c.AllowUnauthenticatedReads))
+			unaryInterceptors = append(unaryInterceptors,
+				server.GRPCmTLSUnaryServerInterceptor(c.AllowUnauthenticatedReads))
 		}
-	}()
+	}
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		idleTimer.ResetTimer()
-		handler(w, r)
-	})
+	if htpasswdSecrets != nil {
+		gba := server.NewGrpcBasicAuth(htpasswdSecrets, c.AllowUnauthenticatedReads)
+		streamInterceptors = append(streamInterceptors, gba.StreamServerInterceptor)
+		unaryInterceptors = append(unaryInterceptors, gba.UnaryServerInterceptor)
+	}
+
+	if idleTimer != nil {
+		it := server.NewGrpcIdleTimer(idleTimer)
+		streamInterceptors = append(streamInterceptors, it.StreamServerInterceptor)
+		unaryInterceptors = append(unaryInterceptors, it.UnaryServerInterceptor)
+	}
+
+	opts = append(opts, grpc.ChainStreamInterceptor(streamInterceptors...))
+	opts = append(opts, grpc.ChainUnaryInterceptor(unaryInterceptors...))
+
+	validateAC := !c.DisableGRPCACDepsCheck
+	validateStatus := "disabled"
+	if validateAC {
+		validateStatus = "enabled"
+	}
+	log.Println("gRPC AC dependency checks:", validateStatus)
+
+	enableRemoteAssetAPI := c.ExperimentalRemoteAssetAPI
+	remoteAssetStatus := "disabled"
+	if enableRemoteAssetAPI {
+		remoteAssetStatus = "enabled"
+	}
+	log.Println("experimental gRPC remote asset API:", remoteAssetStatus)
+
+	network := "tcp"
+	addr := c.GRPCAddress
+	if strings.HasPrefix(c.GRPCAddress, "unix://") {
+		network = "unix"
+		addr = c.GRPCAddress[len("unix://"):]
+	}
+
+	log.Printf("Starting gRPC server on address %s", addr)
+
+	*grpcServer = grpc.NewServer(opts...)
+
+	if !grpcSem.TryAcquire(1) {
+		log.Println("bazel-remote is shutting down, not starting gRPC server")
+		return nil
+	}
+	err3 := server.ListenAndServeGRPC(*grpcServer,
+		network, addr,
+		validateAC,
+		c.EnableACKeyInstanceMangling,
+		enableRemoteAssetAPI,
+		diskCache, c.AccessLogger, c.ErrorLogger)
+	if err3 != nil {
+		log.Fatal("gRPC server returned fatal error:", err3)
+	}
+
+	return err3
 }
 
 // A http.HandlerFunc wrapper which requires successful basic
